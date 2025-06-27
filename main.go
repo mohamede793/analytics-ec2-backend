f293aed9137c038de1bab72da3dd8b2caa0820ac
Mohamed El Ousrouti <melousrouti@gmail.com>
2025-06-27 17:58:54 -0400

    testing

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func sendResponse(w http.ResponseWriter, statusCode int, success bool, message s
 
 // Health check endpoint for ALB
 func healthHandler(w http.ResponseWriter, r *http.Request) {
-	sendResponse(w, http.StatusOK, true, "Service is healthy", nil, "")
+	sendResponse(w, http.StatusOK, true, "this is a test", nil, "")
 }
 
 // Name endpoint - accepts name parameter and returns greeting
